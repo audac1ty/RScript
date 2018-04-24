9033c81855bf446a1eb75ffc80faa055d5e0b09c
Matt Scanlon <matt.n.scanlon@gmail.com>
2018-04-24 15:24:19 +0100

    Update install.sh
    Sed the shit out of this

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-sudo apt-get install unixodbc unixodbc-dev --install-suggests -y
+sudo apt-get install unixodbc* -y
 sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 exit
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+[Get those gcc flags into the sudo vi /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf file]
+sudo chmod o+w /usr/lib64/microsoft-r/3.3/lib64/R/etc
+sudo sed "s/^CXX1X.=.*$/CXX1X = gcc -std=c++0x/" /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf > /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf~
+sudo rm /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf
+sudo sed "s/^CXX1XSTD.=.*$/CXX1XSTD = -std=c++0x -fPIC/" /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf~ > /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf
+sudo rm /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf~
+sudo chmod o-w /usr/lib64/microsoft-r/3.3/lib64/R/etc
 wget https://raw.githubusercontent.com/audac1ty/RScript/e9966ab7be5bd6d1c2b36679fcb72b106dd2e388/install_r.r
 R CMD BATCH install_r.r
