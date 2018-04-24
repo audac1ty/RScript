3a9c095ab46150e01b591adf0b5af39eaf9dbe0c
Matt Scanlon <matt.n.scanlon@gmail.com>
2018-04-24 15:54:12 +0100

    Update install.sh
    Fuck su

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!
 sudo apt-get install unixodbc* -y
-sudo su 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-exit
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17
 sudo chmod o+w /usr/lib64/microsoft-r/3.3/lib64/R/etc
