a8a216b91dbb5305cda8d41f65e030128d20b155
Matt Scanlon <matt.n.scanlon@gmail.com>
2018-04-24 16:13:03 +0100

    Update install.sh
    Updated and SHOULD work

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-#!
-sudo apt-get install unixodbc* -y
-sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+#!/bin/bash
+sudo apt-get install unixodbc* -y 
+sudo wget https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add microsoft.asc
+sudo chmod o+w /etc/apt/sources.list.d/
+sudo wget https://packages.microsoft.com/config/ubuntu/16.04/prod.list 
+sudo mv prod.list /etc/apt/sources.list.d/mssql-release.list
+sudo chmod o-w /etc/apt/sources.list.d/
+sudo chown root /etc/apt/sources.list.d/prod.list
+sudo chgrp root /etc/apt/sources.list.d/prod.list
+sudo chmod g-w /etc/apt/sources.list.d/prod.list
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17 -y
 sudo chmod o+w /usr/lib64/microsoft-r/3.3/lib64/R/etc
 sudo sed "s/^CXX1X.=.*$/CXX1X = gcc -std=c++0x/" /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf > /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf~
 sudo rm /usr/lib64/microsoft-r/3.3/lib64/R/etc/Makeconf
